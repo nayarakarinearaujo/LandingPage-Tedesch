65f3b4fb88ede2cccb142d20f1c6eb28cfa5343f
-Nayara <nayarakarine.araujo@gmail.com>
2024-05-20 12:00:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
+
+    menuToggle.addEventListener('click', function () {
+        navbarMenu.classList.toggle('open');
+    });
+});
